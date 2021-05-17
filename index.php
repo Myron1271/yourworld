0c32bd9902573ce3c4bed474da238262dad0aaa2
Mette <mette.ghijsen@gmail.com>
2021-05-17 10:04:25 +0200

    dingetjes gemaakt

--- index.php
+++ index.php
@@ -268,6 +268,7 @@
                         <li><i class="bx bx-check"></i> Een account voor één persoon</li>
                         <li><i class="bx bx-check"></i> Zelfstandig leren om verantwoordelijk met je geld om te gaan </li>
                         <li><i class="bx bx-check"></i> Te gebruiken op maximaal één apparaat</li>
+                        <br><br><br>
                     </ul>
                     <a href="#" class="get-started-btn">Get Started</a>
                 </div>
@@ -279,7 +280,7 @@
                     <ul>
                         <li><i class="bx bx-check"></i> Studentenaccounts voor een hele klas tot en met 35 personen</li>
                         <li><i class="bx bx-check"></i> In groepsverband leren om verantwoordelijk met je geld om te gaan</li>
-                        <li><i class="bx bx-check"></i> Te gebruiken op maximaal 5 verschillende</li>
+                        <li><i class="bx bx-check"></i> Te gebruiken op maximaal 5 verschillende apparaten</li>
                         <li><i class="bx bx-check"></i> Een docentenaccount als klassebeheerder</li>
                     </ul>
                     <a href="#" class="get-started-btn">Get Started</a>
@@ -289,11 +290,10 @@
                     <h3>Voortgezet onderwijspakket</h3>
                     <h4>€30<span>per maand</span></h4>
                     <ul>
-                        <li><i class="bx bx-check"></i> Quam adipiscing vitae proin</li>
-                        <li><i class="bx bx-check"></i> Nec feugiat nisl pretium</li>
-                        <li><i class="bx bx-check"></i> Nulla at volutpat diam uteera</li>
-                        <li><i class="bx bx-check"></i> Pharetra massa massa ultricies</li>
-                        <li><i class="bx bx-check"></i> Massa ultricies mi quis hendrerit</li>
+                        <li><i class="bx bx-check"></i> Studentenaccounts voor meerdere klassen tot en met 175 personen </li>
+                        <li><i class="bx bx-check"></i> In groepsverband leren om verantwoordelijk met je geld om te gaan</li>
+                        <li><i class="bx bx-check"></i> Te gebruiken op maximaal 5 verschillende apparaten </li>
+                        <li><i class="bx bx-check"></i> 5 docenten accounts als klassenbeheerder</li>
                     </ul>
                     <a href="#" class="get-started-btn">Get Started</a>
                 </div>
@@ -308,18 +308,17 @@
         <div class="container">
 
             <div class="section-title">
-                <h2 data-aos="fade-in">Frequently Asked Questions</h2>
-                <p data-aos="fade-in">Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+                <h2 data-aos="fade-in">Veelgestelde vragen</h2>
             </div>
 
             <div class="row faq-item d-flex align-items-stretch" data-aos="fade-up">
                 <div class="col-lg-5">
                     <i class="bx bx-help-circle"></i>
-                    <h4>Non consectetur a erat nam at lectus urna duis?</h4>
+                    <h4>Kan ik ook spelen zonder account?</h4>
                 </div>
                 <div class="col-lg-7">
                     <p>
-                        Feugiat pretium nibh ipsum consequat. Tempus iaculis urna id volutpat lacus laoreet non curabitur gravida. Venenatis lectus magna fringilla urna porttitor rhoncus dolor purus non.
+                         Nee. Een account wordt gebruikt om jouw data te versturen tussen verschillende apparaten.
                     </p>
                 </div>
             </div><!-- End F.A.Q Item-->
@@ -327,11 +326,11 @@
             <div class="row faq-item d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100">
                 <div class="col-lg-5">
                     <i class="bx bx-help-circle"></i>
-                    <h4>Feugiat scelerisque varius morbi enim nunc faucibus a pellentesque?</h4>
+                    <h4>Hoe wordt mijn bestelling geleverd?</h4>
                 </div>
                 <div class="col-lg-7">
                     <p>
-                        Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi. Id interdum velit laoreet id donec ultrices. Fringilla phasellus faucibus scelerisque eleifend donec pretium. Est pellentesque elit ullamcorper dignissim.
+                        Nadat je een studentenaccount hebt aangemaakt kun je inloggen in de game. Wanneer je een docentenaccount hebt aangemaakt, zullen er activatiecodes voor het aantal bestelde accounts naar je gemaild worden.
                     </p>
                 </div>
             </div><!-- End F.A.Q Item-->
@@ -339,35 +338,14 @@
             <div class="row faq-item d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="200">
                 <div class="col-lg-5">
                     <i class="bx bx-help-circle"></i>
-                    <h4>Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi?</h4>
+                    <h4>Hoeveel mensen kan ik toevoegen aan mijn klas?</h4>
                 </div>
                 <div class="col-lg-7">
                     <p>
-                        Eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis orci. Faucibus pulvinar elementum integer enim. Sem nulla pharetra diam sit amet nisl suscipit. Rutrum tellus pellentesque eu tincidunt. Lectus urna duis convallis convallis tellus.
+                        Bij aankoop van een standaard onderwijspakket krijg je 35 verschillende studentenaccounts. In dit geval kun je dus maximaal 35 verschillende studenten toevoegen aan je klas.
                     </p>
-                </div>
-            </div><!-- End F.A.Q Item-->
-
-            <div class="row faq-item d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="300">
-                <div class="col-lg-5">
-                    <i class="bx bx-help-circle"></i>
-                    <h4>Ac odio tempor orci dapibus. Aliquam eleifend mi in nulla?</h4>
-                </div>
-                <div class="col-lg-7">
                     <p>
-                        Aperiam itaque sit optio et deleniti eos nihil quidem cumque. Voluptas dolorum accusantium sunt sit enim. Provident consequuntur quam aut reiciendis qui rerum dolorem sit odio. Repellat assumenda soluta sunt pariatur error doloribus fuga.
-                    </p>
-                </div>
-            </div><!-- End F.A.Q Item-->
-
-            <div class="row faq-item d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="400">
-                <div class="col-lg-5">
-                    <i class="bx bx-help-circle"></i>
-                    <h4>Tempus quam pellentesque nec nam aliquam sem et tortor consequat?</h4>
-                </div>
-                <div class="col-lg-7">
-                    <p>
-                        Molestie a iaculis at erat pellentesque adipiscing commodo. Dignissim suspendisse in est ante in. Nunc vel risus commodo viverra maecenas accumsan. Sit amet nisl suscipit adipiscing bibendum est. Purus gravida quis blandit turpis cursus in
+                        Bij aankoop van een voortgezet onderwijspakket krijg je 175 verschillende studentenaccounts.In dit geval kun je dus maximaal 125 verschillende studenten toevoegen aan 5 verschillende klassen.
                     </p>
                 </div>
             </div><!-- End F.A.Q Item-->
@@ -418,24 +396,24 @@
                     <form action="forms/contact.php" method="post" role="form" class="php-email-form w-100" data-aos="fade-up">
                         <div class="row">
                             <div class="col-md-6 form-group">
-                                <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required>
+                                <input type="text" name="name" class="form-control" id="name" placeholder="Jouw naam" required>
                             </div>
                             <div class="col-md-6 form-group mt-3 mt-md-0">
-                                <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required>
+                                <input type="email" class="form-control" name="email" id="email" placeholder="Jouw email" required>
                             </div>
                         </div>
                         <div class="form-group mt-3">
-                            <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required>
+                            <input type="text" class="form-control" name="subject" id="subject" placeholder="Onderwerp" required>
                         </div>
                         <div class="form-group mt-3">
-                            <textarea class="form-control" name="message" rows="5" placeholder="Message" required></textarea>
+                            <textarea class="form-control" name="message" rows="5" placeholder="Bericht" required></textarea>
                         </div>
                         <div class="my-3">
                             <div class="loading">Loading</div>
                             <div class="error-message"></div>
-                            <div class="sent-message">Your message has been sent. Thank you!</div>
+                            <div class="sent-message">Jouw bericht is verzonden, dankjewel!</div>
                         </div>
-                        <div class="text-center"><button type="submit">Send Message</button></div>
+                        <div class="text-center"><button type="submit">Verstuur bericht</button></div>
                     </form>
                 </div>
             </div>
