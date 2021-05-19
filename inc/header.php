59f3cbb3822083609836c1287515f7be58085693
Myron <myronmax_seelen@live.nl>
2021-05-20 00:04:41 +0200

    Over ons margin Fix

--- inc/header.php
+++ inc/header.php
@@ -16,7 +16,7 @@
 
         <nav id="navbar" class="navbar">
             <ul>
-                <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+                <li><a class="nav-link scrollto active" href="index.php#hero">Home</a></li>
                 <li><a class="nav-link scrollto" href="#about">Over Ons</a></li>
                 <li><a class="nav-link scrollto" href="#team">Team</a></li>
                 <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
