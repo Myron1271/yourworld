ff4edc31e440d1bc65a74f35a8439e7fa61814ec
Myron <myronmax_seelen@live.nl>
2021-06-18 20:53:06 +0200

    login.func redirect fix

--- func/login.func.php
+++ func/login.func.php
@@ -16,7 +16,7 @@
         if (password_verify($Password_Login, $row['password']))
         {
             $_SESSION['userAdmin'] = $Username_Login;
-            header("Location: http://145.220.75.78/teacher.php");
+            header("Location: yourworldgame.nl/teacher.php");
             exit();
         }
         else echo "Het ingevoerde wachtwoord klopt niet!";
